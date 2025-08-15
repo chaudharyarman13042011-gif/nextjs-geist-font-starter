ef63872ad68c45650c85aa88eb0e3f8afec9762f
chaudharyarman13042011-gif <chaudharyarman13042011@gmail.com>
2025-08-15 18:49:43 +0530

    Add .next/static/chunks/src_app_ludo_page_tsx_a993527f._.js 1755263980105

---
+++ .next/static/chunks/src_app_ludo_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_653324b8._.js",
+  "static/chunks/src_84342ca2._.js"
+],
+    source: "dynamic"
+});
